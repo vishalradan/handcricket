0c5d132b6d9a8400522725ebe3ad94044e3423f1
vishalradan <36251710+vishalradan@users.noreply.github.com>
2022-07-23 18:35:48 +0530

    Add files via upload

---
+++ handcricket.py
@@ -0,0 +1,64 @@
+import random
+
+lst1= [1,2,3,4,5,6]
+
+chances_1=chances_2= int(input('Enter the number of balls'))
+no_of_chances_1= 0
+your_runs= 0
+ 
+print ("-----------------------------------------------\nYour Batting\n")
+while no_of_chances_1 < chances_1:
+    
+    runs= int(input("Enter Runs for Your Batting Turn: "))
+    comp_bowl= random.choice(lst1)
+
+    if runs==comp_bowl:
+        print ("Your Guess: ",runs,",Computer Guess: ",comp_bowl)
+        print ("You are Out. Your Total Runs= ", your_runs,"\n")
+        break
+    elif runs>6:
+        print ("ALERT!! Support No only till 6\n")
+        continue
+    else:
+        your_runs= your_runs+runs
+        print ("Your Guess: ",runs,",Computer Guess: ",comp_bowl)
+        print ("Your runs Now are: ",your_runs,"\n")
+
+    no_of_chances_1= no_of_chances_1 + 1  
+
+lst2= [1,2,3,4,5,6]
+
+no_of_chances_2= 0
+comp_runs= 0
+print ("-----------------------------------------------")
+print ("Computer Batting-\n")
+while no_of_chances_2 < chances_2:
+
+    bowl= int(input("Enter Runs for Your Bowling Turn: "))
+    comp_bat= random.choice(lst2)
+
+    if comp_bat==bowl:
+        print ("Computer Guess: ",comp_bat,"Your Guess: ",bowl)
+        print ("The Computer is Out. Computer Runs= ",comp_runs,"\n")
+        break
+    else:
+        comp_runs= comp_runs+comp_bat
+        print ("Computer Guess: ",comp_bat,"Your Guess: ",bowl)
+        print ("Computer Runs: ",comp_runs,"\n")
+
+        if comp_runs > your_runs:
+            break
+        
+    no_of_chances_2= no_of_chances_2+1
+
+
+print ("\n-----------------------------------------------\nRESULTS: ")
+
+if comp_runs < your_runs:
+    print ("\nYou won the Game.\n\nYour Total Runs= ",your_runs,"  [Bowls taken: ",no_of_chances_1+1,"]","\nComputer Total Runs= ",comp_runs,"  [Bowls Taken: ",no_of_chances_2+1,"]\n")
+
+elif comp_runs == your_runs:
+    print ("The Game is a Tie")
+
+else:
+    print("\nComputer won the Game.\n\nComputer Total Runs= ",comp_runs,"  [Bowls Taken: ",no_of_chances_2+1,"]","\nYour Total Runs= ",your_runs,"  [Bowls taken: ",no_of_chances_1+1,"]\n")
